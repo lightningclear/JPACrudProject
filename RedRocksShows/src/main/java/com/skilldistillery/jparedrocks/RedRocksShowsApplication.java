a038bcf0db3aef76350ef8054a0c01b600e390db
lightningclear <voh777@yahoo.com>
2022-04-17 19:06:14 -0600

    Started boot mvc project with Jpa project dependency

---
+++ RedRocksShows/src/main/java/com/skilldistillery/jparedrocks/RedRocksShowsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jparedrocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedRocksShowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedRocksShowsApplication.class, args);
+	}
+
+}
---
+++ RedRocksShows/src/test/java/com/skilldistillery/jparedrocks/RedRocksShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jparedrocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedRocksShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
